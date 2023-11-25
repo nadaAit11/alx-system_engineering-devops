bb5948ec075c8a2705cbdc432aa45fdb80e0b011
nada <nadaait171@gmail.com>
2023-11-24 16:27:59 -0800

    add tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,9 @@
 # Using Puppet, install flask from pip3.
 
 # Install pip3 package
-  package { 'python3-pip':
-    ensure => present,
-  }
+package { 'python3-pip':
+  ensure => present,
+}
 
 # Use pip3 to install Flask version 2.1.0
 package { 'Flask':
