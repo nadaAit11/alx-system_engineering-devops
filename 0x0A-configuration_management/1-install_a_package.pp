dc9b7c2325b4bbd6c94cd6c91190000e3dcfeb5a
nada <nadaait171@gmail.com>
2023-12-03 17:04:02 -0800

    instll

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,18 @@
-# 1-install_a_package.pp
-
 # Ensure pip3 is installed
 package { 'python3-pip':
   ensure => present,
 }
 
-# Install flask from pip3 with version 2.1.0
+# Install Flask from pip3 with version 2.1.0
 package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Ensure specific version of Werkzeug is installed
+package { 'Werkzeug':
+  ensure   => '2.1.1', # or the version compatible with Flask 2.1.0
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
